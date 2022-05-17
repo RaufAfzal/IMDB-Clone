097b38bae2c3e992ebf6ee0fe4fa303e35bc1548
Rauf Afzal <Rufiwattoo@gmail.com>
2022-05-17 11:17:58 +0500

    Initial pull request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   def add_to_watchlist
     return @add_to_watchlist_exist ? "Remove from the watchlist" : "watchlist"
-    
+
   end
 
   include Pundit::Authorization
@@ -13,6 +13,7 @@ class ApplicationController < ActionController::Base
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
     redirect_back(fallback_location: root_path)
+    
   end
 
   helper_method :add_to_watchlist
